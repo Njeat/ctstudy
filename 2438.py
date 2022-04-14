664bd309dc1a094ee82e52d5e3e893f1d31c0e51
parktj <ekswn120@gmail.com>
2022-04-14 21:22:58 +0900

    2443

--- 2438.py
+++ 2438.py
@@ -1,9 +1,10 @@
 # 별찍기 - 1
 
+from re import I
 import sys
 sys.stdin = open("00.txt")
 
 n = int(input())
 
 for i in range(1,n+1):
-    print(' '* (n-i) + '*' * i + '*' * (i-1))
+    print(' '* (i-1) + '*' * (n-(i-1)) + '*' * (n-i))
