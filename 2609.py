97053d66941a38aa6479cdc78e25c7d9abd0f887
parktj <ekswn120@gmail.com>
2022-04-20 00:38:37 +0900

    2609 need to study

--- 2609.py
+++ 2609.py
@@ -4,15 +4,3 @@ sys.stdin = open("00.txt")
 
 n, m = map(int,input().split())
 print(n,m)
-max = []
-i = 0
-# while True:
-#     if n // i == m // i:
-#         max.append(i)
-    
-    
-while True:
-    i += 1
-    if n * i == m * i:
-        print(n*i)
-        break
