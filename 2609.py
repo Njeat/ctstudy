95ec38e23a84265d77896598a317f7a2c5d9d7b9
parktj <ekswn120@gmail.com>
2022-04-21 21:14:36 +0900

    2609

--- 2609.py
+++ 2609.py
@@ -3,4 +3,14 @@ import sys
 sys.stdin = open("00.txt")
 
 n, m = map(int,input().split())
-print(n,m)
+
+def gcd(n, m):
+    while m > 0:
+        n, m = m, n%m
+    return n
+
+def lcm(n, m):
+    return n*m//gcd(n,m)
+
+print(gcd(n,m))
+print(lcm(n,m))
